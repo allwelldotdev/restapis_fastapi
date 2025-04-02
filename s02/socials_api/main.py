499a80fec0a946b68c728b1019a1e4b621d2635e
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-04-02 09:54:53 +0100

    codebase restructure

--- s02/socials_api/main.py
+++ s02/socials_api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from .routes import user_comments, user_posts
+from .api.routes.user_comments import router as user_comments
+from .api.routes.user_posts import router as user_posts
 
 app = FastAPI()
 
@@ -10,5 +11,5 @@ async def read_root():
     return {"Hello": "World!"}
 
 
-app.include_router(user_posts.router)
-app.include_router(user_comments.router)
+app.include_router(user_posts)
+app.include_router(user_comments)
